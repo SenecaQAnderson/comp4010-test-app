55f2ab0e030072e14435e60088271e4bfc37eb94
SenecaQAnderson <senecaqanderson@gmail.com>
2021-09-23 09:26:18 -0400

    Moved code from Navbar.tsx to App.tsx and deleted Navbar.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
 import logo from './logo.svg';
-import Navbar from './components/Navbar';
+import React, { useState } from 'react';
+import {
+  Page,
+  PageHeader,
+  PageHeaderTools,
+  PageSidebar,
+  PageSection,
+  PageSectionVariants
+} from '@patternfly/react-core';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-    </div>
-  );
+  const [isNavOpen, setNavOpen] = useState(true);
+    const onNavToggle = () => {
+        setNavOpen(!isNavOpen);
+    }
+    const logoProps = {
+        href: 'https://patternfly.org',
+        onClick: () => console.log('clicked logo'),
+        target: '_blank'
+      };
+
+      const Header = (
+        <PageHeader
+          logo="Logo"
+          logoProps={logoProps}
+          headerTools={<PageHeaderTools>header-tools</PageHeaderTools>}
+          showNavToggle
+          isNavOpen={isNavOpen}
+          onNavToggle={onNavToggle}
+        />
+      );
+      const Sidebar = <PageSidebar nav="Navigation" isNavOpen={isNavOpen} />;
+  
+      return (
+        <div className="App">
+        <Page header={Header} sidebar={Sidebar}>
+          <PageSection variant={PageSectionVariants.darker}>Section with darker background</PageSection>
+          <PageSection variant={PageSectionVariants.dark}>Section with dark background</PageSection>
+          <PageSection variant={PageSectionVariants.light}>Section with light background</PageSection>
+        </Page>
+        </div>
+      );
 }
 
 export default App;
